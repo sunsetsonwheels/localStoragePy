be86c2da7050aa7491bd762390ce20fe150f98be
jkelol111 <vietbetatester@outlook.com>
2019-03-01 14:15:56 +0800

    Changed name and added PyPI script.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='localStoragePy',
+      version='0.1',
+      description='A familiar API from the Web, adapted to storing data locally with Python.',
+      url='http://github.com/jkelol111/localStoragePy',
+      author='Nguyen Thanh Nam (jkelol111)',
+      long_description=long_description,
+      author_email='vietbetatester@outlook.com',
+      license='MIT',
+      packages=['localStoragePy'],
+      classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+      ],
+      zip_safe=False
+)
+
